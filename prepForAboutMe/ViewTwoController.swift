dd4f15a6fa4cbc85377e5e76542f5c8cef9b2480
snoupyrelete <drob8896@740s-pl-112.local>
2016-10-03 07:56:33 -0600

    Setup ViewTwoController and linked to storyboard

---
+++ prepForAboutMe/ViewTwoController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewTwo.swift
+//  prepForAboutMe
+//
+//  Created by Robson, Dylan on 10/3/16.
+//  Copyright © 2016 Robson, Dylan. All rights reserved.
+//
+
+import UIKit
+
+class viewTwoController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    
+    
+    }
+    
+}
